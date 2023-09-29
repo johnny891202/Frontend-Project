b8a15e3a7bb0a2af3d39b2dd6eeed56ffb7dd2a7
柯維哲 <johnny54328322@gmail.com>
2023-09-29 10:53:41 +0800

    update

--- 一變應萬變的響應式網頁設計/最終作業scss版本/js/all.js
+++ 一變應萬變的響應式網頁設計/最終作業scss版本/js/all.js
@@ -7,4 +7,21 @@ $(document).ready(function () {
         e.preventDefault();
         $(this).toggleClass("fas");
     });
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= 20){
+            $(".scroll-to-top").fadeIn(250);
+        }
+        else {
+            $(".scroll-to-top").fadeOut(250);
+        }
+    });
+    $(".scroll-to-top").click(function (e) {
+        e.preventDefault();
+        $("html, body").animate(
+            {
+                scrollTop: 0,
+            },
+            500
+        );
+    });
 });
